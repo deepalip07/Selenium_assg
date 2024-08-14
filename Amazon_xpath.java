75eb3f05c89c77725dadfe8e5b3b5a0a1d1675fb
unknown <deepalip071992@gmail.com>
2024-08-15 00:11:57 +0530

    Amazon search box using relative xpath

---
+++ Amazon_xpath.java
@@ -0,0 +1,22 @@
+package Pkg1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Amazon_xpath 
+{
+
+	public static void main(String[] args) 
+	{
+	  ChromeDriver driver=new ChromeDriver();
+	  
+	  driver.get("https://www.amazon.in/");
+	 WebElement search= driver.findElement(By.xpath("(//input)[5]"));
+	 
+	 search.sendKeys("Shoes");
+	 search.sendKeys(Keys.ENTER);
+	}
+
+}
