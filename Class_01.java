9208766e41dfd1d81678d4f331cbbc29b22a4547
unknown <deepalip071992@gmail.com>
2024-08-15 00:13:48 +0530

    Selenium practice

---
+++ Class_01.java
@@ -0,0 +1,14 @@
+package Pkg1;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Class_01 {
+
+	public static void main(String[] args) 
+	{
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://www.linkedin.com");
+
+	}
+
+}
