3e7376292a1955758306cec084fbfd35afab50b3
unknown <deepalip071992@gmail.com>
2024-08-17 16:26:39 +0530

    Launching chrome,firefox,edge browser

---
+++ Launch_all_browsers.java
@@ -0,0 +1,18 @@
+package Pkg1;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+public class Launch_all_browsers {
+
+	public static void main(String[] args)
+	{
+		WebDriver driver1 = new FirefoxDriver();
+		WebDriver driver2 = new EdgeDriver();
+		ChromeDriver driver3=new ChromeDriver();
+		
+	}
+
+}
