81deb7d53804a613db7926242089ee99e0cd3af8
unknown <deepalip071992@gmail.com>
2024-08-20 02:37:39 +0530

    Search 4th autosugetion in amazon for fridge

---
+++ Amazon_search_4th_autosugestion_fridge.java
@@ -0,0 +1,30 @@
+package Pkg1;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Amazon_search_4th_autosugestion_fridge 
+{
+	public static void main(String[] args) throws InterruptedException 
+	{
+		ChromeDriver driver=new ChromeDriver();
+		  
+        driver.get("https://www.amazon.in");
+		  driver.manage().window().maximize();
+		  WebElement e1=driver.findElement(By.id("twotabsearchtextbox"));
+		  e1.sendKeys("fridge");
+		  Thread.sleep(1000);
+
+		  List<WebElement> e2= driver.findElements(By.xpath("//div[@class='s-suggestion-container']"));
+			 
+			 int count =e2.size();
+			 System.out.println(count);
+			 e2.get(3).click();
+	}
+
+	}
+
+
